5e074d75997d1329cf9f89231760ff7c224b5216
WangPingChun <wangpc14@chinaunicom.cn>
2018-12-13 18:39:21 +0800

    Initial commit

---
+++ src/main/java/wang/crispin/example/drools/chapter2/Drools5ApiTest.java
@@ -0,0 +1,15 @@
+package wang.crispin.example.drools.chapter2;
+
+import org.junit.Test;
+
+/**
+ * @author WangPingChun
+ * @since 2018-12-13
+ */
+public class Drools5ApiTest {
+
+    @Test
+    public void testDrools5Api() {
+
+    }
+}
---
+++ src/main/java/wang/crispin/example/drools/model/Goods.java
@@ -0,0 +1,15 @@
+package wang.crispin.example.drools.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author WangPingChun
+ * @since 2018-12-13
+ */
+@Getter
+@Setter
+public class Goods {
+    private int discount = 100;
+}
